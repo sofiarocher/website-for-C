0356ac1d92c553f976ffa703de9548709278ad15
Sofía Rocher <sofirocher_@hotmail.com>
2024-06-22 12:43:51 -0300

    fixed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import LaminadosOnline from './courses/LaminadosOnline'
-import Formacion from './pages/Formacion'
-import Home from "./pages/Home"
-import Online from './pages/Online'
-import Presenciales from './pages/Presenciales'
-import SobreMi from './pages/SobreMi'
-
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LaminadosOnline from './courses/LaminadosOnline';
+import Formacion from './pages/Formacion';
+import Home from "./pages/Home";
+import Online from './pages/Online';
+import Presenciales from './pages/Presenciales';
+import SobreMi from './pages/SobreMi';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' Component={Home} />
-          <Route path='/sobremi' Component={SobreMi} />
-          <Route path='/online' Component={Online} />
-          <Route path='/presenciales' Component={Presenciales} />
-          <Route path='/formacion' Component={Formacion} />
-          <Route path='/curso-de-laminados-veganos-online' Component={LaminadosOnline}></Route>
+          <Route path='/' element={<Home />} />
+          <Route path='/sobremi' element={<SobreMi />} />
+          <Route path='/online' element={<Online />} />
+          <Route path='/presenciales' element={<Presenciales />} />
+          <Route path='/formacion' element={<Formacion />} />
+          <Route path='/curso-de-laminados-veganos-online' element={<LaminadosOnline />} />
         </Routes>
       </Router>
+      <Analytics />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {NextUIProvider} from "@nextui-org/react";
-import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <NextUIProvider>
       <React.StrictMode>
         <App />
-        <Analytics />
       </React.StrictMode>
     </NextUIProvider>
 )
